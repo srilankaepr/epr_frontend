514b85c803973d87377ad27f0a5e0dea4d719273
Dumidu Himsara <dumiduhimsara19@gmail.com>
2026-03-16 18:34:52 +0530

    Updated axios with credentials for secure cookies

--- src/api.js
+++ src/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // මුළු project එකටම එකම base URL එක මෙතන තියෙන්නේ
 const API = axios.create({
-    baseURL: 'https://eprbackend-production.up.railway.app/api'
+    baseURL: 'https://eprbackend-production.up.railway.app/api',
+    withCredentials: true
 });
 
 export default API;
