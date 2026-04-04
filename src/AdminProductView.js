1c785698c3d9ba87f51c85d76ddbe21c432a970a
Dumidu Himsara <dumiduhimsara19@gmail.com>
2026-04-04 22:07:49 +0530

    Frontend security and auth update

--- src/AdminProductView.js
+++ src/AdminProductView.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import API from './api';
 import { useNavigate } from 'react-router-dom';
 import jsPDF from 'jspdf';
