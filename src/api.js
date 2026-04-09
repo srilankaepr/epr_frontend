a06106d6e35ce003640de58195fd81282ae82236
Dumidu Himsara <dumiduhimsara19@gmail.com>
2026-04-09 14:35:11 +0530

    Frontend security and auth update

--- src/api.js
+++ src/api.js
@@ -1,13 +1,13 @@
-/*import axios from 'axios';
+import axios from 'axios';
 
 const API = axios.create({
     baseURL: process.env.REACT_APP_API_URL || 'https://eprbackend-production.up.railway.app/api'
 });
 
-export default API; */
+export default API; 
 
 
-
+/*
 import axios from 'axios';
 
 const API = axios.create({
@@ -29,3 +29,5 @@ API.interceptors.request.use((config) => {
 });
 
 export default API;
+
+*/
