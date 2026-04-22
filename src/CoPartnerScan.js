8c18a3ea60c05860019b7a44a98ed0b322e1cab6
Dumidu Himsara <srilankaepr@gmail.com>
2026-04-23 02:05:36 +0530

    Updated axios with credentials for secure cookies

--- src/CoPartnerScan.js
+++ src/CoPartnerScan.js
@@ -162,7 +162,7 @@ return (
               border: '1px solid #333',
               background: '#080808',
               color: '#fff',
-              fontSize: isMobile ? '10px' : '18px',
+              fontSize: isMobile ? '12px' : '18px',
               textAlign: 'center',
               outline: 'none',
               transition: '0.3s',
