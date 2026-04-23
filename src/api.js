a73d7650261768aef7ba689aed6721e463ce0be8
Dumidu Himsara <srilankaepr@gmail.com>
2026-04-23 14:27:40 +0530

    Updated axios with credentials for secure cookies

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'https://eprbackend-production-6318.up.railway.app'
+    baseURL: process.env.REACT_APP_API_URL 
 });
 
 //...............................................................
