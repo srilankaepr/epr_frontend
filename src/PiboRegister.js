2adb0218840d5a60b5b0713d660b62acb26ddc24
srilankaepr <srilankaepr@gmail.com>
2026-09-23 16:01:49 +0530

    final clean cors config

--- src/PiboRegister.js
+++ src/PiboRegister.js
@@ -122,8 +122,8 @@ const PiboRegister = () => {
         setIsLoading(true);
 
         try {
-           // const response = await API.post('/customers/register', finalPayload);
-            const response = await API.post('/pibo/register', finalPayload);
+            const response = await API.post('/customers/register', finalPayload);
+            // const response = await API.post('/pibo/register', finalPayload);
             if (response.status === 201 || response.status === 200) {
                 alert("✅ PIBO Smart Registration Protocol Transmitted and Executed Successfully!");
                 navigate('/');
